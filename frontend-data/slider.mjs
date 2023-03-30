2788c2121548e7a359665e7998850514eee452ad
Sabir-itobuz <sabir@itobuz.com>
2023-03-30 12:49:10 +0530

    few more changes

--- frontend-data/slider.mjs
+++ frontend-data/slider.mjs
@@ -5,9 +5,12 @@ export const slide = [
     price: "From 150$ /night",
     roomDesc:
       "with wide and comfortable areas and daylight pouring in from the..",
-    image: "https://svgshare.com/i/rXA.svg",
+
+    image:
+      "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
   },
   {
-    image: "https://svgshare.com/i/rVo.svg`",
+    image:
+      "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
   },
 ];
--- frontend-data/top.mjs
+++ frontend-data/top.mjs
@@ -2,6 +2,7 @@ export const top = [
   {
     heading: "Enjoy Your Dream Vacations",
     desc: "Quiet and endlessly beautiful hideway",
-    image: "https://svgshare.com/i/rVo.svg ",
+    image:
+      "https://images.unsplash.com/photo-1611048267451-e6ed903d4a38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2944&q=80",
   },
 ];
